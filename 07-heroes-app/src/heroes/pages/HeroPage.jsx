bf203af45b585a3baf928cb724bfef0df3e16b92
= <=>
2023-10-21 09:12:08 -0600

    Estilo de hero page y use memo

--- 07-heroes-app/src/heroes/pages/HeroPage.jsx
+++ 07-heroes-app/src/heroes/pages/HeroPage.jsx
@@ -1,19 +1,44 @@
-import React from 'react'
-import { Navigate, useParams } from 'react-router-dom'
+import React, { useMemo } from 'react'
+import { Navigate, useNavigate, useParams } from 'react-router-dom'
 import { getHeroById } from '../helpers/getHeroById'
 
 export const HeroPage = () => {
 
   const { heroId } = useParams()
 
-  const hero = getHeroById(heroId)
+  const hero = useMemo(() => getHeroById(heroId), [heroId])
+
+  const navigate = useNavigate()
+
+  const onNavigateBack = () => {
+    navigate(-1)
+  }
 
   if (!hero) return <Navigate to="/marvel" />
 
-
-  console.log(hero)
-
   return (
-    <h1>HeroPage</h1>
+    <div className="row mt5">
+      <div className="col-4">
+        <img
+          src={`/assets/heroes/${heroId}.jpg`}
+          alt={hero.superhero}
+          className="img-thumbnail" />
+      </div>
+      <div className="col-8">
+        <h3>{hero.superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item list-group-item-dark"><b>Alter ego:</b> {hero.alter_ego}</li>
+          <li className="list-group-item list-group-item-dark"><b>Publisher:</b> {hero.publisher}</li>
+          <li className="list-group-item list-group-item-dark"><b>First appearance:</b> {hero.first_appearance}</li>
+        </ul>
+        <h5 className="mt-3">Characters</h5>
+        <p>{hero.characters}</p>
+
+        <button className="btn btn-outline-info"
+          onClick={() => onNavigateBack()}>
+          Regresar
+        </button>
+      </div>
+    </div>
   )
 }
