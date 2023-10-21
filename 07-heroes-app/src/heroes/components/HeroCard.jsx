a8a0d53a69ff82838dbd962a87a73eb9e98df100
= <=>
2023-10-21 08:44:07 -0600

    Leer argumentos por URL

--- 07-heroes-app/src/heroes/components/HeroCard.jsx
+++ 07-heroes-app/src/heroes/components/HeroCard.jsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom"
+
+const CharactersByHero = ({alter_ego, characters}) => {
+    if(alter_ego === characters) return (<></>)
+
+    return <p>{characters}</p>
+}
 
 export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters }) => {
 
@@ -5,17 +12,26 @@ export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance
 
     return (
         <div className="col">
-            <div className="card">
+            <div className="card bg-dark text-white">
                 <div className="row no-gutters">
                     <div className="col-4">
                         <img src={heroImageUrl} className="card-img" alt={superhero} />
                     </div>
                     <div className="col-8">
-                        <div className="card-body">
+                        <div className="card-body text-white">
                             <div className="card-title">{superhero}</div>
                             <div className="card-text">{alter_ego}</div>
 
-                            <p>{characters}</p>
+                            <CharactersByHero characters={characters} alter_ego={alter_ego} />
+
+                            <p className="card-text">
+                                <small className="text-light">{first_appearance}</small>
+                            </p>
+
+                            <Link to={`/heroe/${id}`}>
+                                Mas..
+                            </Link>
+
                         </div>
                     </div>
                 </div>
