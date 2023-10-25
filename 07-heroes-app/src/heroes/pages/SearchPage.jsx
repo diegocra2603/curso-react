e1d6efd7690e5db91660203dbd0e171cd384a128
= <=>
2023-10-25 16:02:15 -0600

    Se agrego condicionales al mensaje de busqueda

--- 07-heroes-app/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-app/src/heroes/pages/SearchPage.jsx
@@ -13,6 +13,9 @@ export const SearchPage = () => {
   const { q = '' } = queryString.parse(location.search)
   const heroes = getHeroesByName(q)
 
+  const showSearch = (q.trim().length !== 0)
+  const showErrorSearch = (q.length > 0) && heroes.length === 0
+
   const { formState, onInputChange, onResetForm } = useForm({
     searchText: ''
   })
@@ -22,7 +25,7 @@ export const SearchPage = () => {
   const onSearchSubmit = (e) => {
     e.preventDefault()
 
-    if (searchText.trim().length <= 1) return
+    // if (searchText.trim().length <= 1) return
 
     navigate(`?q=${searchText}`)
 
@@ -58,12 +61,20 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
 
-          <div className="alert alert-primary">
+          {/* {
+            (q === '')
+              ? <div className="alert alert-primary"> Search a hero </div>
+              : (heroes.length === 0) && <div className="alert alert-danger">No hero with  <b>{q}</b>
+              </div>
+          } */}
+
+          <div className="alert alert-primary animate__animated animate__fadeIn" style={{ display: showSearch ? 'none' : '' }}>
             Search a hero
           </div>
 
-          <div className="alert alert-danger">
-            No hero with  <b>{q}</b>
+          <div className="alert alert-danger animate__animated animate__fadeIn" style={{ display: showErrorSearch ? '' : 'none' }}>
+            No hero with  <b>{q}
+            </b>
           </div>
 
           {
