eb0879f984e541846ec6cd27f2d30473ef3c0f4b
= <=>
2023-10-26 16:08:06 -0600

    Instalando Material UI

--- 08-journal-app/src/main.jsx
+++ 08-journal-app/src/main.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './styles.css'
 import { JournalApp } from './JournalApp'
 import { BrowserRouter } from 'react-router-dom'
+import './styles.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
