94020a0b54d6defa9c6624364f87a1623374d2fb
= <=>
2023-10-26 14:58:53 -0600

    Rutas privadas

--- 07-heroes-app/src/routes/AppRouter.jsx
+++ 07-heroes-app/src/routes/AppRouter.jsx
@@ -3,13 +3,21 @@ import { Route, Routes } from 'react-router-dom'
 
 import { HeroesRoutes } from '../heroes'
 import { LoginPage } from '../auth'
+import { PrivateRouter } from './PrivateRouter'
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
         <Route path='login' element={<LoginPage />} />
-        <Route path='/*' element={<HeroesRoutes />} />
+
+        <Route path='/*' element={
+          <PrivateRouter>
+            <HeroesRoutes />
+          </PrivateRouter>
+        }/>
+
+        {/* <Route path='/*' element={<HeroesRoutes />} /> */}
       </Routes>
     </>
   )
