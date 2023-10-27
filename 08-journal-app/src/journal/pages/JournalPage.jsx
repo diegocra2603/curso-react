d066a8c2ed230ae01a2099ad72d4b0e3c2119caf
= <=>
2023-10-27 10:28:58 -0600

    JournalLayout y JournalPage

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,9 +1,12 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography variant='h1'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel quaerat cumque pariatur inventore. Quod asperiores assumenda minima, delectus aliquam consequatur sit id. Ratione consequatur tempora, odit pariatur vitae nisi voluptatum!</Typography>
+      {/* NothingSelected */}
+      {/* NoteView */}
+    </JournalLayout>
   )
 }
