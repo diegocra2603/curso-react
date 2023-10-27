ba1e7c25e3967534d49d1e98ade0356a809ba3c6
= <=>
2023-10-27 12:46:43 -0600

    Noteviewd

---
+++ 08-journal-app/src/journal/views/NoteView.jsx
@@ -0,0 +1,52 @@
+import { SaveOutlined } from "@mui/icons-material"
+import { Button, Grid, TextField, Typography } from "@mui/material"
+import { ImageGallery } from "../components"
+
+export const NoteView = () => {
+    return (
+        <Grid
+            container
+            direction="row"
+            justifyContent="space-between"
+            sx={{ mb: 1 }}
+            gap={2}
+        >
+
+            <Grid item>
+                <Typography fontSize={39}>28 de agosto de 2023</Typography>
+            </Grid>
+
+            <Grid item>
+                <Button color="primary" sx={{ padding: 2 }} >
+                    <SaveOutlined sx={{ padding: 2 }} />
+                    Guardar
+                </Button>
+            </Grid>
+
+            <Grid container>
+                <TextField 
+                type="text"
+                variant="filled"
+                fullWidth
+                placeholder="Ingres un título"
+                label="Título"
+                />
+            </Grid>
+
+            <Grid container>
+                <TextField 
+                type="text"
+                variant="filled"
+                fullWidth
+                multiline
+                placeholder="¿Qué sucedió en el día de hoy?"
+                minRows={ 5 }
+                />
+            </Grid>
+
+            {/* ImagenGalery */}
+            <ImageGallery />
+
+        </Grid>
+    )
+}
