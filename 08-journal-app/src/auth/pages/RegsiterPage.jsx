86ce28c5747a927ef5789f39d001b5c3360f5128
= <=>
2023-10-27 10:21:07 -0600

    Register Page - Diseño

--- 08-journal-app/src/auth/pages/RegsiterPage.jsx
+++ 08-journal-app/src/auth/pages/RegsiterPage.jsx
@@ -1,5 +1,61 @@
+import { Link as RouterLink } from "react-router-dom"
+import { Google } from "@mui/icons-material"
+import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { AuthLayout } from "../layout/AuthLayout"
+
 export const RegsiterPage = () => {
   return (
-    <div>RegsiterPage</div>
+
+    <AuthLayout title="Register">
+      <form>
+        <Grid container gap={1} >
+
+          <Grid item xs={12}>
+            <TextField
+              label="Full Name"
+              type="text"
+              placeholder="Full name"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              label="Email"
+              type="email"
+              placeholder="email@google.com"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              label="Password"
+              type="password"
+              placeholder="Password"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container spacing={1}  fullWidth>
+            <Grid item xs={12}>
+              <Button variant="contained" fullWidth>
+                Crear cuenta
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Typography sx={{mr:1}} >¿Ya tienes una cuenta?</Typography>
+            <Link component={RouterLink} color="inherit" to="/auth/login">
+              Ingresar
+            </Link>
+
+          </Grid>
+
+        </Grid>
+      </form>
+    </AuthLayout>
+
   )
 }
