daa92c514ef56c7aade2a55aeaef5f793830c669
= <=>
2023-10-27 20:27:08 -0600

    Mostrar los Pokemones paginadamente

--- 09-toolkit-redux/src/PokemonApp.jsx
+++ 09-toolkit-redux/src/PokemonApp.jsx
@@ -1,25 +1,32 @@
 import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { getPokemons } from "./store/slices/pokemon"
 
 export const PokemonApp = () => {
+
+    const { pokemons, isLoading, page } = useSelector(state => state.pokemons)
 
     const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(getPokemons())
     }, [])
-    
 
     return (
         <>
-        <h1>Pokemon App</h1>
-        <hr />
-        <ul>
-            <li>Hola</li>
-            <li>Hola</li>
-            <li>Hola</li>
-        </ul>
+            <h1>Pokemon App</h1>
+            <span>Loading: {isLoading ? 'True' : 'False'}</span>
+            <hr />
+            <ul>
+                {pokemons.map(({ name }) => (
+                    <li key={name}>{name}</li>
+
+                ))}
+            </ul>
+
+            <button disabled={isLoading} onClick={() => dispatch(getPokemons(page + 1))}>
+                Next
+            </button>
         </>
     )
 }
