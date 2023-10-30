d21e4a9c31f572d2ef06c9a8c85a6983b9d91b94
= <=>
2023-10-30 09:14:23 -0600

    Consumir apis mediante el custom hook

--- 09-toolkit-redux/src/TodoApp.jsx
+++ 09-toolkit-redux/src/TodoApp.jsx
@@ -1,11 +1,22 @@
+import { useGetTodosQuery } from "./store/apis"
+
 export const TodoApp = () => {
+
+    const {data : todos = [], isLoading } = useGetTodosQuery()
+
   return (
     <>
         <h1>Todos - RTK Query</h1>
         <hr />
-        <h4>isLoading...</h4>
+        <h4>isLoading: { isLoading ? 'True' : 'False'}</h4>
         
         <pre>...</pre>
+
+        <ul>
+            {todos.map( ({id, title, completed}) => (
+                <li  key={id}><strong>{completed ? 'DONE' : 'Pending'}</strong>{title}</li>
+            ))}
+        </ul>
 
         <button>
             Next Todo
