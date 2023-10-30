110d451d334e1553fe8aa5f182ee9b6b25970453
= <=>
2023-10-30 10:08:58 -0600

    Configurando Redux en nuestra aplicacion

--- 08-journal-app/src/main.jsx
+++ 08-journal-app/src/main.jsx
@@ -7,11 +7,15 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <JournalApp />
-    </BrowserRouter>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
