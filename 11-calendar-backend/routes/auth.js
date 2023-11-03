c079a1d4a769792db2a451429e3b98c9c7a9c2e8
= <=>
2023-11-03 16:40:12 -0600

    Expres validator

--- 11-calendar-backend/routes/auth.js
+++ 11-calendar-backend/routes/auth.js
@@ -4,12 +4,26 @@
 */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
 const router = Router();
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 
-router.post('/new', crearUsuario)
+router.post(
+    '/new',
+    [ //Midelwares
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'Debe ser un correo electronico valido').isEmail(),
+        check('password', 'El Password debe de ser de 6 caracteres').isLength(6)
+    ],
+    crearUsuario)
 
-router.post('/', loginUsuario)
+router.post(
+    '/',
+    [
+        check('email', 'Debe ser un correo electronico valido').isEmail(),
+        check('password', 'El Password debe de ser de 6 caracteres').isLength(6)
+    ],
+    loginUsuario)
 
 router.get('/renew', revalidarToken)
 
