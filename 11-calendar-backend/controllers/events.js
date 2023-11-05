ead874d67ccb8efa8b12aca22d57024dac7bc16f
= <=>
2023-11-04 18:28:41 -0600

    Se valido los mensajes de error

--- 11-calendar-backend/controllers/events.js
+++ 11-calendar-backend/controllers/events.js
@@ -56,7 +56,7 @@ const actualizarEvento = async (req, res = response) => {
         if (evento.user.toString() !== uid) {
             return res.status(401).json({
                 ok: false,
-                msg: 'No tiene privilegio de editar este evento'
+                msg: 'No tiene privilegio de interactuar con este evento'
             })
         }
 
@@ -101,7 +101,7 @@ const eliminarEvento = async(req, res = response) => {
         if (evento.user.toString() !== uid) {
             return res.status(401).json({
                 ok: false,
-                msg: 'No tiene privilegio de editar este evento'
+                msg: 'No tiene privilegio de interactuar con este evento'
             })
         }
 
